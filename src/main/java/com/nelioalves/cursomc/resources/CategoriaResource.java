f3ff0ec03d512c09cdc51db08eca07ce4f566876
Selmo Silva <selmoronaldo@hotmail.com>
2023-02-03 09:19:27 -0300

    v.2.05 - Paginação com parâmetros opcionais na requisição

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -2,15 +2,16 @@ package com.nelioalves.cursomc.resources;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -67,6 +68,22 @@ public class CategoriaResource {
 		// Tem que ter um "constructor CategoriaDTO" que pegue os dados necessários do "constructor Categoria"
 		// stream (collection) - map (mapear obj, chamando função) - collect (transforma o obj em uma lista)
 		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDTO);
+	}	
+	
+	// service.findPage
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		
+		// Come é um Page, e não uma List, pode tirar o "steam" e o "collect"
+		Page<CategoriaDTO> listDTO = list.map(obj -> new CategoriaDTO(obj));
 		
 		return ResponseEntity.ok().body(listDTO);
 	}	
--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
@@ -49,6 +52,14 @@ public class CategoriaService {
 	// 2) Use o padrão DTO para poder escolher os campos desejados. Como por exemplo categoria sem os produtos
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	// O findPage abaixo chama o repo.findAll
+	public Page<Categoria> findPage(Integer numberPage, Integer linesPerPage, String orderBy, String direction) {
+		
+		PageRequest pageRequest = PageRequest.of(numberPage, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		return repo.findAll(pageRequest);
 	}
 
 }
