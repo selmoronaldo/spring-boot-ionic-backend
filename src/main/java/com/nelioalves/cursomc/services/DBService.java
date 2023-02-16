8cc2ddaa1c13213c2a721f3de2dd6deba44bd95f
Selmo Silva <selmoronaldo@hotmail.com>
2023-02-16 08:48:03 -0300

    s.05.065 - Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/nelioalves/cursomc/services/DBService.java
+++ src/main/java/com/nelioalves/cursomc/services/DBService.java
@@ -113,7 +113,7 @@ public class DBService {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		// Cliente / Endereço
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "selmo.ronaldo@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
